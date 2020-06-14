adbfdd9c43ed145526c5da19030a028fa60af2a6
Lorenzo Ceccon <lorenzo.ceccon@studenti.unipd.it>
2020-06-14 17:27:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Project create with terraform 0.12.26
+
+1) Clone project
+2) terraform init
+3) Create terraform file for store variables => touch terraform.tfvars
+4) terraform plan
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default = "eu-west-1"
+}
+
+variable "AMIS" {
+    type = map(string)
+    default = {
+        eu-west-1 = "ami-089cc16f7f08c4457"
+    }
+}
